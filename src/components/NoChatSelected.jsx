12064c6e26c29ac2f3a282a58e689f7c413c5644
Puneet Kushwaha <148247654+puneetkushwaha@users.noreply.github.com>
2026-02-18 23:36:11 +0530

    Fix: Remove duplicated code and repair build

--- src/components/NoChatSelected.jsx
+++ src/components/NoChatSelected.jsx
@@ -6,7 +6,7 @@ const NoChatSelected = () => {
             <div className="flex flex-col items-center text-center space-y-6">
                 {/* Illustration */}
                 <div className="w-64 h-64 bg-transparent relative">
-                    <img src="/logo.jpg" alt="PingMe Web" className="w-full h-full object-contain opacity-40 grayscale" />
+                    <img src="/logo.png" alt="PingMe Web" className="w-full h-full object-contain opacity-40" />
                 </div>
 
                 {/* Text Content */}
